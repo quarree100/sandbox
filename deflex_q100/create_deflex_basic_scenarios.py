57c5d4f3da26581954d2eed00a6742314836559c
uvchik <uvchik.git@posteo.eu>
2020-07-06 17:03:09 +0200

    Add script to create basic scenarios

---
+++ deflex_q100/create_deflex_basic_scenarios.py
@@ -0,0 +1,16 @@
+from deflex import basic_scenario, config as cfg
+from oemof.tools import logger
+
+years = [2014, 2013, 2012]
+# years = [2013]
+rmaps = ["de22", "de21", "de17", "de02"]
+# rmaps = ['de02']
+logger.define_logging()
+
+for year in years:
+    for rmap in rmaps:
+        for heat in [True, False]:
+            for group in [True, False]:
+                cfg.tmp_set("basic", "heat", heat)
+                cfg.tmp_set("basic", "group_transformer", group)
+                basic_scenario.create_basic_scenario(year, rmap)
